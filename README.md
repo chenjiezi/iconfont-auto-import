edf3ca5df657c056e6c408337bf5ab159f8db348
chenjiezi <892774940@qq.com>
2022-07-14 19:15:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# auto-import-iconfont
---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * auto import iconfont for node
+ */
+class AutoImportIconfont {
+  constructor(config) {
+
+  }
+
+  start() {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+}
+
+module.exports = AutoImportIconfont;
+
